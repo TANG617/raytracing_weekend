c9e0f1d470bf46b28953168138a4e9cd54ce2df8
LiTang <litang0617@outlook.com>
2024-08-24 19:50:31 +0800

    finished 6.2

--- main.cpp
+++ main.cpp
@@ -10,17 +10,25 @@
 //Compute a color for the closest intersection point.
 
 
-bool hit_sphere(const point3& center, double radius, const ray& r){
+double hit_sphere(const point3& center, double radius, const ray& r){ //calculate t instead of whether hit
     vec3 oc = center - r.origin();
     auto a = dot(r.direction(), r.direction());
     auto b = -2.0 * dot(r.direction(), oc);
     auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
-    return (discriminant >= 0);
+    auto discriminant = b*b - 4*a*c;// NO simplification
+
+    if (discriminant < 0) return -1.0;
+    else return (-b - std::sqrt(discriminant) ) / (2.0*a);
 }
 
 color ray_color(const ray& r) {
-    if(hit_sphere(point3(0,0,-1),0.5,r)) return color (1,0,0);
+//    if(hit_sphere(point3(0,0,-1),0.5,r)) return color (1,0,0);
+
+    auto t = hit_sphere(point3(0,0,-1),0.5,r);
+    if(t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1)); // N is normalized vector pointing from center of sphere to the hit point
+        return color(0.6 + 0.4 * dot(N,-r.direction()),0,0);
+    }
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
     return (1.0-a)*color (1.0,1.0,1.0)+a*color (0.5,0.7,1.0);
