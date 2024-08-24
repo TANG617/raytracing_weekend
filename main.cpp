064a9630dc2d00e41101f0bdba5c6fc418f1c783
LiTang <litang0617@outlook.com>
2024-08-24 19:34:25 +0800

    finished 5.0 red sphere

--- main.cpp
+++ main.cpp
@@ -10,17 +10,28 @@
 //Compute a color for the closest intersection point.
 
 
+bool hit_sphere(const point3& center, double radius, const ray& r){
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant >= 0);
+}
+
 color ray_color(const ray& r) {
+    if(hit_sphere(point3(0,0,-1),0.5,r)) return color (1,0,0);
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
     return (1.0-a)*color (1.0,1.0,1.0)+a*color (0.5,0.7,1.0);
 }
 
+
 int main() {
 
     // Image
     auto aspect_ratio = 16.0/9.0;
-    int image_width = 400;
+    int image_width = 2160;
     int image_height = int(image_width / aspect_ratio);
         // image_height = (image_height < 1) ? 1 : image_height;
 
