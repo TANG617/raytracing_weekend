2a0067173a98726b5b8c2438151002d56cf7d3dc
LiTang <litang0617@outlook.com>
2024-08-27 14:16:36 +0800

    finish c10.5

--- camera.h
+++ camera.h
@@ -8,6 +8,7 @@
 #include "rtweekend.h"
 #include "fstream"
 #include "hittable.h"
+#include "material.h"
 
 class camera {
 public:
@@ -101,19 +102,29 @@ private:
         return ray(ray_origin,ray_direction);
     }
 
-    color ray_color(const ray& r,int depth,  const hittable& world) const {
+    color ray_color(const ray& r, int depth,  const hittable& world) const {
         if(depth<=0) return color(0,0,0);
         hit_record rec;
 
-        if (world.hit(r, interval(0.001, infinity), rec)) {
+        if (world.hit(r, interval(0.01, infinity), rec)) {
 //            return 0.5 * (rec.normal + color(1,1,1));
 //            vec3 direction = random_on_hemisphere(rec.normal);
-            vec3 direction = rec.normal + random_unit_vector();
-            return 0.5 * ray_color(ray(rec.p,direction),depth-1,world);
+
+//            vec3 direction = rec.normal + random_unit_vector();
+//            return 0.5 * ray_color(ray(rec.p,direction),depth-1,world);
+
+            ray scattered;
+            color attenuation;
+//            rec.mat = std::make_shared<material>(/* 构造函数参数 */);
+            if (rec.mat->scatter(r, rec, attenuation, scattered))
+                return attenuation * ray_color(scattered, depth-1, world);
+
+
+            return color(0,0,0);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
-        auto a = 0.5*(unit_direction.y() + 1.0);
+        auto a = 0.1*(unit_direction.y() + 1.0);
         return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
     }
 };
--- sphere.h
+++ sphere.h
@@ -10,7 +10,10 @@
 
 class sphere : public hittable {
 public:
-    sphere(const point3& center, double radius) : center(center), radius(std::fmax(0,radius)) {}
+//    sphere(const point3& center, double radius) : center(center), radius(std::fmax(0,radius)) {}
+    sphere(const point3& center, double radius, shared_ptr<material> mat)
+            : center(center), radius(std::fmax(0,radius)), mat(mat) {}
+
 
     bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override { //Calculate whether hit and hit point
         vec3 oc = center - r.origin();
@@ -35,6 +38,7 @@ public:
         rec.p = r.at(rec.t);//hit point
         vec3 outward_normal = (rec.p - center) / radius; //Normal vector for the hit point
         rec.set_face_normal(r, outward_normal);
+        rec.mat = mat;
 
         return true;
     }
@@ -62,6 +66,7 @@ public:
         rec.p = r.at(rec.t);//hit point
         vec3 outward_normal = (rec.p - center) / radius; //Normal vector for the hit point
         rec.set_face_normal(r, outward_normal);
+        rec.mat = mat;
 
         return true;
     }
@@ -69,6 +74,7 @@ public:
 private:
     point3 center;
     double radius;
+    shared_ptr<material> mat;
 };
 
 #endif
